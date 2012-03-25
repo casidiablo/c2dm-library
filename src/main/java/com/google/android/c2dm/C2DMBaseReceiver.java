d94a8abaf1409cf802901c42288ba308ef9547a2
Cristian <cristian@elhacker.net>
2012-03-25 15:47:05 -0500

    Renamed package

--- src/main/java/com/google/android/c2dm/C2DMBaseReceiver.java
+++ src/main/java/com/google/android/c2dm/C2DMBaseReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.codeslap.c2dm;
+package com.google.android.c2dm;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
--- src/main/java/com/google/android/c2dm/C2DMBroadcastReceiver.java
+++ src/main/java/com/google/android/c2dm/C2DMBroadcastReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.codeslap.c2dm;
+package com.google.android.c2dm;
 
 import android.app.Activity;
 import android.content.BroadcastReceiver;
